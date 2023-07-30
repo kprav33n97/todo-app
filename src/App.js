c8ab111015494e811b944654d40d7930e1118bf4
kprav33n97 <kpraveenkumar2006@gmail.com>
2023-07-30 21:59:13 +0530

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function TodoItem({ item, toggleTask, deleteTask }) {
+  return (
+    <li className={`task ${item.completed ? 'completed' : ''}`}>
+      <span onClick={() => toggleTask(item.id)}>{item.text}</span>
+      <button onClick={() => deleteTask(item.id)}>Delete</button>
+    </li>
+  );
+}
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const addTask = () => {
+    if (inputValue.trim() !== '') {
+      setTasks([
+        ...tasks,
+        {
+          id: Date.now(),
+          text: inputValue,
+          completed: false,
+        },
+      ]);
+      setInputValue('');
+    }
+  };
+
+  const toggleTask = (taskId) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter a task..."
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <TodoItem
+            key={task.id}
+            item={task}
+            toggleTask={toggleTask}
+            deleteTask={deleteTask}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
