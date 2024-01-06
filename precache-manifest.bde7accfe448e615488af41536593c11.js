9dc6930394d3de3e45a1bba8737a9770788ca096
kprav33n97 <kpraveenkumar2006@gmail.com>
2024-01-06 16:00:30 +0530

    Updates

---
+++ precache-manifest.bde7accfe448e615488af41536593c11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f641a57504127ea976a100e533d850",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "40ee1f6f33f066e792af",
+    "url": "/todo-app/static/css/main.e4affe4f.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/todo-app/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/todo-app/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "40ee1f6f33f066e792af",
+    "url": "/todo-app/static/js/main.95d26ff4.chunk.js"
+  },
+  {
+    "revision": "c01e25032b6cc4a6bf48",
+    "url": "/todo-app/static/js/runtime~main.b929bfaa.js"
+  }
+]);
